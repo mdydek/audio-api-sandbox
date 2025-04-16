27a4817ecfb80f955e0143ed76588ab07cb92099
michal <dydmichal@gmail.com>
2025-04-16 10:04:49 +0200

    init commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,101 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { AudioContext, AudioBuffer } from 'react-native-audio-api';
+import React, { FC, useEffect, useRef } from 'react';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+type KeyName = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G';
+const Keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G'] as const;
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+type PR<T> = Partial<Record<KeyName, T>>;
+
+interface ButtonProps {
+  keyName: KeyName;
+  onPressIn: (key: KeyName) => void;
+  onPressOut: (key: KeyName) => void;
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+const sourceList: PR<string> = {
+  A: 'https://github.com/fuhton/piano-mp3/raw/refs/heads/master/piano-mp3/A4.mp3',
+  B: 'https://github.com/fuhton/piano-mp3/raw/refs/heads/master/piano-mp3/B4.mp3',
+  C: 'https://github.com/fuhton/piano-mp3/raw/refs/heads/master/piano-mp3/C4.mp3',
+  D: 'https://github.com/fuhton/piano-mp3/raw/refs/heads/master/piano-mp3/D4.mp3',
+  E: 'https://github.com/fuhton/piano-mp3/raw/refs/heads/master/piano-mp3/E4.mp3',
+  F: 'https://github.com/fuhton/piano-mp3/raw/refs/heads/master/piano-mp3/F4.mp3',
+  G: 'https://github.com/fuhton/piano-mp3/raw/refs/heads/master/piano-mp3/G4.mp3',
+};
+
+const Button = ({ onPressIn, onPressOut, keyName }: ButtonProps) => (
+  <Pressable
+    onPressIn={() => onPressIn(keyName)}
+    onPressOut={() => onPressOut(keyName)}
+    style={({ pressed }) => ({
+      margin: 4,
+      padding: 12,
+      borderRadius: 2,
+      backgroundColor: pressed ? '#d2e6ff' : '#abcdef',
+    })}
+  >
+    <Text style={{ color: 'white' }}>{`${keyName}`}</Text>
+  </Pressable>
+);
+
+const SimplePiano: FC = () => {
+  const audioContextRef = useRef<AudioContext | null>(null);
+  const bufferMapRef = useRef<PR<AudioBuffer>>({});
+
+  const onKeyPressIn = (which: KeyName) => {
+    const audioContext = audioContextRef.current;
+    let buffer = bufferMapRef.current[which];
+
+    if (!audioContext || !buffer) {
+      return;
+    }
+
+    const source = audioContext.createBufferSource();
+    source.buffer = buffer;
+
+    source.connect(audioContext.destination);
+    source.start();
+  };
+
+  const onKeyPressOut = (which: KeyName) => {};
+
+  useEffect(() => {
+    if (!audioContextRef.current) {
+      audioContextRef.current = new AudioContext();
+    }
+
+    Object.entries(sourceList).forEach(async ([key, url]) => {
+      bufferMapRef.current[key as KeyName] = await fetch(url)
+        .then((response) => response.arrayBuffer())
+        .then((arrayBuffer) =>
+          audioContextRef.current!.decodeAudioData(arrayBuffer)
+        );
+    });
+
+    return () => {
+      audioContextRef.current?.close();
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+      }}
+    >
+      {Keys.map((key) => (
+        <Button
+          onPressIn={onKeyPressIn}
+          onPressOut={onKeyPressOut}
+          keyName={key}
+          key={key}
+        />
+      ))}
+    </View>
+  );
+};
+
+export default SimplePiano;
